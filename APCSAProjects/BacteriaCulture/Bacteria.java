581c1045840f608889b1d0ad4403c58a63da260e
Disguised-Coffee <68920714+Disguised-Coffee@users.noreply.github.com>
2022-06-14 13:23:41 -0500

    Create Bacteria.java
    Recursive

---
+++ APCSAProjects/BacteriaCulture/Bacteria.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Bacteria
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        
+        System.out.println("How many hours will the bacteria breed? ");
+        int hours = input.nextInt();
+        int bacteria = colonySize(hours);
+        System.out.println("After " + hours + " hours, there will be " + bacteria);
+    }
+    
+    public static int colonySize(int hour)
+    {
+        // Write a base case
+        if(hour == 0)
+            return 7;
+        if(hour == 1)
+        {
+            return 7 + 7 * 4;
+        }
+        
+        // Write a recursive call
+        return colonySize(hour -1 ) + colonySize(hour - 1) * 4;
+    }
+
+}
